46e3432a24b95ccaa8b473eabe109bd75a4bb3cf
FriendlyIndr <abhyudaya.tiwari003@gmail.com>
2026-03-09 21:51:54 +0530

    Rectangles rendered with rounded corners

--- wobbleboard/src/scene/shapes/rectangle.ts
+++ wobbleboard/src/scene/shapes/rectangle.ts
@@ -3,11 +3,28 @@ import type { Shape } from "./types";
 
 export const rectangleShape: Shape = {
     render(rc, element) {
-        rc.rectangle(
-            element.x,
-            element.y,
-            element.width,
-            element.height,
+        const { x, y } = element;
+
+        const width = Math.abs(element.width);
+        const height = Math.abs(element.height);
+
+        const r = Math.min(element.width, element.height) * 0.2;
+
+        const path = `
+            M ${x + r} ${y}
+            L ${x + width - r} ${y}
+            A ${r} ${r} 0 0 1 ${x + width} ${y + r}
+            L ${x + width} ${y + height - r}
+            A ${r} ${r} 0 0 1 ${x + width - r} ${y + height}
+            L ${x + r} ${y + height}
+            A ${r} ${r} 0 0 1 ${x} ${y + height - r}
+            L ${x} ${y + r}
+            A ${r} ${r} 0 0 1 ${x + r} ${y}
+            Z
+        `;
+
+        rc.path(
+            path,
             {
                 roughness: 1.5,
                 stroke: "black",
